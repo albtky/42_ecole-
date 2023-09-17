3f5b1996739fa5563456034cf86e88b21767857a
Alperen Tokay <atokay@k1m15s07.42istanbul.com.tr>
2023-09-17 04:01:36 +0300

    atoi düzeltildi 0403

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,31 +6,48 @@
 /*   By: atokay <atokay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 12:03:43 by atokay            #+#    #+#             */
-/*   Updated: 2023/09/14 09:33:18 by atokay           ###   ########.fr       */
+/*   Updated: 2023/09/17 03:57:43 by atokay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *str)
+int	whitespaces(char *str, int *ptr_i)
 {
+	int	count;
 	int	i;
-	int	negative;
-	int	number;
 
 	i = 0;
-	negative = 0;
-	number = 0;
-	while ((str[i] != '\0') && (str[i] < 48 || str[i] > 57))
+	count = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] && (str[i] == 43 || str[i] == 45))
 	{
-		if (str[i] == '-')
-			negative++;
+		if (str[i] == 45)
+			count *= -1;
 		i++;
 	}
-	while (str[i] >= 48 && str[i] <= 57)
+	*ptr_i = i;
+	return (count);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	result = 0;
+	sign = whitespaces(str, &i);
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
 	{
-		number = number * 10 + str[i] - 48;
+		result *= 10;
+		result += str[i] - 48;
 		i++;
 	}
-	if (negative % 2 == 1)
-		return (number * -1);
-	return (number);
+	result *= sign;
+	return (result);
+}
+int main(void)
+{
+	char *s = "   ---+--+01234506789ab567";
+	printf("%d", ft_atoi(s));
 }
