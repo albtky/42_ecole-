efae8d13db8b8c0f4ca0e59f7df9bc860f1cf94d
Alperen Tokay <alperenttky@gmail.com>
2023-09-23 18:15:36 +0300

    THE END

--- sınav/aff-a/aff_a.c
+++ sınav/aff-a/aff_a.c
@@ -4,42 +4,12 @@
 // last_word ++
 // first_word ++
 // rot13 ++
-// itoa
-// atoi
+// itoa ++
+// atoi ++
 #include <stdio.h>
 #include <stdlib.h>
 
-int ftatoi(const char *s)
+int main(int ac, char *av[])
 {
-    int i = 0;
-    int result = 1;
-    int sign = 0;
-
-    while (s[i] == 32 || s[i] == '\t')
-        i++;
-
-    if (s[i] == 45)
-    {
-        result *= -1;
-        i++;
-    }
-    else if (s[i] == 43)
-        i++;
-    while (!(s[i] >= '0' && s[i] <= '9' && s[i]))
-        i++;    
-
-    while (s[i] >= '0' && s[i] <= '9' && s[i])
-    {
-        sign *= 10;
-        sign += s[i] - '0';
-        i++;
-    }
-    return (sign * result);
-}
-int main(void)
-{
-    char *set = "  -+1231312lkasnda";
-    int a = ftatoi(set);
-    printf("%d\n", a);
-    return (0);
+    
 }
---
+++ sınav/atoi1/fonksıyon.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int main(int ac, char *av[])
+{
+    if (ac == 2)
+    {
+        int i = 0;
+        while (av[1])
+        {
+            if ((av[1][i] >= 'A' && av[1][i] <= 'M') || (av[1][i] >= 'a' && av[1][i] <= 'm'))
+            {
+                av[][]
+            }
+        }
+    }
+}
